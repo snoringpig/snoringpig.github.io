9d8c8650f5c27bcc87f9932d80863654e5471f02
snoringpig <snoringpig99@gmail.com>
2020-10-31 21:19:01 +0800

    Use a hacky way to force the image to stay vertical: add '_v' at the end of the image name

--- thumbnail.py
+++ thumbnail.py
@@ -4,6 +4,7 @@ import os.path
 import math
 from PIL import Image, ExifTags
 
+debug = False
 scale = 0.2
 f_original = os.getcwd() + "/assets/img/original/"
 f_thumbnail = os.getcwd() + "/assets/img/thumbnail/"
@@ -11,7 +12,12 @@ f_thumbnail = os.getcwd() + "/assets/img/thumbnail/"
 for file in os.listdir(f_original):
 	f_img = f_original + file
 	img = Image.open(f_img)
+	if os.path.splitext(f_img)[0][-2:] == "_v":
+		img = img.rotate(270, expand=True)
+		if debug:
+			print(f_img + " rotated!")
 	img.thumbnail((400,400))
 	#img = img.resize((math.ceil(img.size[0] * scale), math.ceil(img.size[1] * scale)), Image.ANTIALIAS)
 	img.save(f_thumbnail + file)
-	print(f_img + " converted")
+	if debug:
+		print(f_img + " converted")
